564a794d90dd63027766423068e37eb6a52a7d24
Joe Sembler <joeysembler@gmail.com>
2021-11-24 10:51:14 -0500

    commit tree d596fb58162cf73d0285f68eb58e494b090e5e80

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+  
+  function superbowlWin(record){
+    const found = record.find(isW);
+    if (found != null)
+    {
+        return found.year;
+    }
+    else{
+        return found;
+    }
+  }
+
+
+  function isW(record){
+    return record.result === 'W';
+  }
